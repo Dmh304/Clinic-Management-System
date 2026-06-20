362423bae805e9a6e1d2542f04ede91fd5762a65
Tridintism23 <tridintstudio23@gmail.com>
2026-06-21 02:38:23 +0700

    TuanTD - 02:37 Sun - 21/06/2026
    triển khai các lớp entity, repository, dto, service, controller cho các chức năng liên quan đến đo mắt tại phòng lab

---
+++ backend/src/main/java/com/ecms/dto/response/LabOrderResponse.java
@@ -0,0 +1,40 @@
+package com.ecms.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.ecms.entity.LabOrderStatus;
+import com.ecms.entity.LabPriority;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LabOrderResponse {
+    private Long id;
+
+    private Long medicalRecordId;
+
+    private String doctorFullName;
+
+    private String labTechnicianFullName;
+
+    private String patientFullName;
+
+    private String notes;
+
+    private LabPriority priority;
+
+    private LabOrderStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime completedAt;
+
+}
--- backend/src/main/java/com/ecms/repository/LabOrderRepository.java
+++ backend/src/main/java/com/ecms/repository/LabOrderRepository.java
@@ -1,4 +1,18 @@
 package com.ecms.repository;
 
-public interface LabOrderRepository {
+import java.util.function.LongSupplier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecms.entity.LabOrder;
+import java.util.List;
+
+public interface LabOrderRepository extends JpaRepository<LabOrder, Long> {
+    List<LabOrder> findByMedicalRecord_PatientId(Long patientId);
+
+    List<LabOrder> findByMedicalRecord_DoctorId(Long doctorId);
+
+    List<LabOrder> findByMedicalRecordId(Long medicalRecordId);
+
+    List<LabOrder> findByLabTechnicianId(Long labTechnicianId);
 }
